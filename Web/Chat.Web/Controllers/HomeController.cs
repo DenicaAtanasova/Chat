5c7cc7c5113b326ebcc379a0b0ee414d7c90c1ab
Denica Atanasova <d.d.atanasova@gmail.com>
2020-08-18 13:42:25 +0300

    Added chat view

--- Web/Chat.Web/Controllers/HomeController.cs
+++ Web/Chat.Web/Controllers/HomeController.cs
@@ -1,29 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Chat.Web.Models;
-
-namespace Chat.Web.Controllers
+﻿namespace Chat.Web.Controllers
 {
+    using System.Diagnostics;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using Chat.Web.Models;
+
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Chat()
         {
             return View();
         }
